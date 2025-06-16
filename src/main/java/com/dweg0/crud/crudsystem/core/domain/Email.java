90e09f6272b4a642c84f3ce3b6d0942e17c4e0a8
dwego <dweg0epic@outlook.com>
2025-06-16 11:06:36 -0300

    feat(email): create Email class with validation and unit test

--- src/main/java/com/dweg0/crud/crudsystem/core/domain/Email.java
+++ src/main/java/com/dweg0/crud/crudsystem/core/domain/Email.java
@@ -1,4 +1,25 @@
 package com.dweg0.crud.crudsystem.core.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Email {
+    private static final String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private String email;
+
+    public Email(String email) {
+        setEmail(email);
+    }
+
+    public void setEmail(String email) {
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+        this.email = email;
+    }
+
+    public static boolean isValidEmail(String email) {
+        return email != null && email.matches(emailRegex);
+    }
+
 }
--- src/test/java/com/dweg0/crud/crudsystem/EmailTest.java
+++ src/test/java/com/dweg0/crud/crudsystem/EmailTest.java
@@ -1,4 +1,23 @@
 package com.dweg0.crud.crudsystem;
 
+import com.dweg0.crud.crudsystem.core.domain.Email;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class EmailTest {
+
+    @Test
+    void testEmail() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Email(""));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Email("test"));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Email(null));
+    }
+
 }
