97e0485dd44362c2375dcfc9a23ffb4f60882dd0
dwego <dweg0epic@outlook.com>
2025-06-16 09:41:06 -0300

    Initial commit

---
+++ src/test/java/com/dweg0/crud/crudsystem/CrudSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dweg0.crud.crudsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
