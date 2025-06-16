1e5a9f58e4f3d61f477570b0ba59452c17d697b5
dwego <dweg0epic@outlook.com>
2025-06-16 19:42:30 -0300

    refactor(user): align User class with coding standards and enhance code quality

--- src/main/java/com/dweg0/crud/crudsystem/core/domain/User.java
+++ src/main/java/com/dweg0/crud/crudsystem/core/domain/User.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -11,6 +12,32 @@ import java.util.List;
 public class User {
     private String name;
     private Email email;
-    private String password;
+    private Password password;
     private List<Order> orders = new ArrayList<>();
+
+    public void addOrder(Order order) {
+        if (orders.contains(order)) {
+            throw new IllegalArgumentException("Order already exists");
+        }
+        orders.add(order);
+    }
+
+    public void removeOrder(Order order) {
+        if (!orders.contains(order)) {
+            throw new IllegalArgumentException("Order does not exist");
+        }
+        orders.remove(order);
+    }
+
+    public List<Order> getOrders() {
+        return Collections.unmodifiableList(orders);
+    }
+
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email.getEmail() + '\'' +
+                ", orders=" + getOrders() +
+                '}';
+    }
 }
