b558c138ecb942261eea12e0aa47a40e73b55092
dwego <dweg0epic@outlook.com>
2025-06-16 19:34:36 -0300

    refactor(model): update classes to follow standards and improve code quality

--- src/main/java/com/dweg0/crud/crudsystem/core/domain/Order.java
+++ src/main/java/com/dweg0/crud/crudsystem/core/domain/Order.java
@@ -1,16 +1,60 @@
 package com.dweg0.crud.crudsystem.core.domain;
 
 import com.dweg0.crud.crudsystem.core.domain.state.OrderStatus;
+import com.dweg0.crud.crudsystem.core.domain.state.Pending;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
-@Setter
 public class Order {
-    private User user;
-    List<Product> products = new ArrayList<>();
+    private final UUID id;
+    private final User user;
+    private final List<OrderItem> items;
+
+    @Setter
     OrderStatus status;
+
+    public Order(User user, List<OrderItem> items) {
+        this.id = UUID.randomUUID();
+        this.user = user;
+        this.items = new ArrayList<>(items);
+        this.status = new Pending();
+    }
+
+    public void addItem(OrderItem item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        this.items.remove(item);
+    }
+
+    public void cancel() {
+        status.cancel(this);
+    }
+
+    public void ship() {
+        status.ship(this);
+    }
+
+    public void deliver() {
+        status.deliver(this);
+    }
+
+    public List<OrderItem> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+
+    public BigDecimal getTotal() {
+        return items.stream()
+                .map(OrderItem::getTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
--- src/main/java/com/dweg0/crud/crudsystem/core/domain/Product.java
+++ src/main/java/com/dweg0/crud/crudsystem/core/domain/Product.java
@@ -1,15 +1,25 @@
 package com.dweg0.crud.crudsystem.core.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @Getter
-@AllArgsConstructor
 public class Product {
+    private final UUID id;
     private final String name;
     private final String description;
-    private final double price;
+    private final BigDecimal price;
     private int stock;
+
+    public Product(String name, String description, BigDecimal price, int stock) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
 
     public void decreaseStock(int amount) {
         stock-=amount;
